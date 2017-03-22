e644acd0a909bb1704f2eb1e2e5fd74631c1b4ab
Juan Pablo Flores Galindo <juanfg@outlook.com>
2017-03-22 14:38:30 -0600

    Cambios en el sidebar y vistas dependiendo del rol del usuario

--- novusPev/app/Http/Controllers/AdminController.php
+++ novusPev/app/Http/Controllers/AdminController.php
@@ -18,86 +18,24 @@ class AdminController extends Controller
     {
         return view('Admin.index', ['usuarios' => User::all()]);
     }
-
-    public function assignRole(Request $request)
+    
+    public function assignRoles(Request $request)
     {
-
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-
         $usuario = User::where('email', $request['email'])->first();
-        $usuario->revokeAllRoles();
-        if ($request['administrador'])
-            $usuario->assignRole('administrador');
-        if ($request['director'])
-            $usuario->assignRole('director');
-        if ($request['pev'])
-            $usuario->assignRole('pev');
-        if ($request['usuario_normal'])
-            $usuario->assignRole('usuario_normal');
+        $roles = Role::select('slug')->get();
+        foreach ($roles as $role) 
+        {
+            if ($request[($role->slug)])
+            {
+                if (!$usuario->hasRole($role->slug))
+                    $usuario->assignRole($role->slug);
+            }
+            else
+            {
+                if ($usuario->hasRole($role->slug))
+                    $usuario->revokeRole($role->slug);
+            }
+        }
         return redirect()->route('admin.index');
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
     }
 }
--- novusPev/routes/web.php
+++ novusPev/routes/web.php
@@ -19,49 +19,42 @@ Route::group(['middleware' => 'auth'], function() {
         return view('/home');
     });
 
-    Route::group([
-            // 'prefix' => 'administrador', 
+    Route::group([ 
+            'prefix' => 'admin',
             'middleware' => ['auth', 'acl'],
             'is' => 'administrador',
-            ], 
-    function () {
-        Route::resource('admin', 'AdminController');
+            ], function () {
+        Route::get('/', 'AdminController@index')->name('admin.index');
+        Route::post('assignRoles', 'AdminController@assignRoles')->name('admin.assignRoles');
     });
 
-    Route::resource('profesores', 'ProfesorController');
+    Route::group([
+        'middleware' => ['auth', 'acl'],
+        'is' => 'administrador|director'
+    ], function() {
+        Route::resource('horarios', 'HorarioController');
+        Route::resource('directores', 'DirectorController');
+        Route::resource('campus', 'CampusController');
+        Route::resource('paises', 'PaisController');
+        Route::resource('areasInteres', 'AreaInteresController');
+        Route::resource('horarios', 'HorarioController');
+        Route::resource('departamentos', 'DepartamentoController');
+        Route::resource('idiomas', 'IdiomaController');
+        Route::resource('materias', 'MateriaController');
+        Route::resource('periodos', 'PeriodoController');
+        Route::resource('semestres', 'SemestreController');
+        Route::resource('evaluaciones', 'EvaluacionController');
+        Route::resource('areasInteres', 'AreaInteresController');
+    });
 
-    Route::resource('directores', 'DirectorController');
-
-    Route::resource('campus', 'CampusController');
-
-    Route::resource('paises', 'PaisController');
-
-
-    Route::resource('areasInteres', 'AreaInteresController');
-
-    Route::resource('profesoresAreasInteres', 'ProfesoresAreaDeInteresController');
-
-    Route::resource('horarios', 'HorarioController');
-
-    Route::resource('departamentos', 'DepartamentoController');
-  
-    Route::resource('idiomas', 'IdiomaController');
-
-    Route::resource('materias', 'MateriaController');
-
-    Route::resource('periodos', 'PeriodoController');
-
-    Route::resource('semestres', 'SemestreController');
-
-
-    Route::resource('evaluaciones', 'EvaluacionController');
-
-    Route::resource('areasInteres', 'AreaInteresController');
+    Route::group([
+    'middleware' => ['auth', 'acl'],
+        'is' => 'administrador|director|profesor'
+    ], function() {
+        Route::resource('profesores', 'ProfesorController');
+        Route::resource('profesoresAreasInteres', 'ProfesoresAreaDeInteresController');
+    });
  
-    Route::resource('profesoresAreasInteres', 'ProfesoresAreaDeInteresController');
- 
-    Route::resource('horarios', 'HorarioController');
-
     Auth::routes();
 
     Route::get('logout', function(){
